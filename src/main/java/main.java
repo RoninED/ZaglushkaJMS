231e370b02683c7d67d40e57a70c224ff357c1ff
John Doe <zavrazka6@mail.ru>
2018-11-07 16:16:55 +0300

    1

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,10 @@
 public class main {
     public static void main(String[] args) {
 
+        AffableThread mFirstThread = new AffableThread("whazzzup", "Hi mark");
         AffableThread mSecondThread = new AffableThread("Hi mark", "whazzzup");    //Создание потока
-        mSecondThread.start();                                  //Запуск потока
+         mSecondThread.start();                                  //Запуск потока
+        mFirstThread.start();
 
         System.out.println("Главный поток завершён...");
     }
